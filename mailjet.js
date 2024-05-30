24688a256ed0d32189a1c203c84675ce07a5f805
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:50:51 -0500

    Update mailjet.js

--- mailjet.js
+++ mailjet.js
@@ -1,1 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const {checkJwtToken} = require('../utils/jwtMiddleware')
+const mailjet = require('node-mailjet').apiConnect(
+    process.env.MAILJET_API_KEY,
+    process.env.MAILJET_PRIVATE_KEY
+)
 
+router.post('/send-message', checkJwtToken, async(req,res)=>{
+    try {
+        const request = mailjet
+                .post('send', { version: 'v3.1' })
+                .request({
+                  Messages: [
+                    {
+                      From: {
+                        Email: "edwinjr38125@gmail.com",
+                        Name: "Edwin Thomas Jr"
+                      },
+                      To: [
+                        {
+                          Email: req.body.recipient,
+                          Name: "You"
+                        }
+                      ],
+                      Subject: "Check out this story!",
+                      TextPart: req.body.message,
+                    }
+                  ]
+                })
+                const result = await request
+                res.json(result.body)
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+
+
+module.exports = router
