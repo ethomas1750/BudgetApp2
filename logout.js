15cc525e93b1334cfab827c7bdadbfe70948d61f
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:24:47 -0500

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,1 +1,18 @@
+// rrd imports
+import {redirect} from" react-router-dom"; Calculating
 
+// library
+import {toast} from "react-toastify";
+
+//helper
+import { deleteItem } from "../helpers";
+
+export async function logoutAction(){
+    // delete the user
+    deleteItem({
+       key: "userName"
+    })
+    toast.success("You've deleted your account")
+    // return redirect
+    return redirect("/")
+}
