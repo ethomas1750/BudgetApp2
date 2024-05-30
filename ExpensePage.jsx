5e2d02d23d4180d347dea6f5c468c32d4bf734c3
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:17:03 -0500

    Update ExpensePage.jsx

--- ExpensePage.jsx
+++ ExpensePage.jsx
@@ -1,1 +1,33 @@
+// rrd imports
+import {userLoaderData} from "react-router-dom"
+
+// helpers
+import {fetchData} from "../helpers";
+
+// loader
+export function dashboardLoader() {
+    const expenses = fetchData("expenses")
+    return{expenses}
+    } 
+
+export const ExpensesPage = () => {
+    const {expenses} = userLoaderData()
+
+    return <div className="grid-lg">
+        <h1>All Expenses</h1>
+        {
+            expenses && expenses.length > 0
+            ? (
+                <div className="grid-md">
+                    <h2>Recent Expenses<small>({expenses.length}total)</small>
+                    </h2>
+                    <Table expenses={expenses}/>
+                    </div>
+            )
+            : <p>No Expenses to show</p>
+        }
+    </div>;
+};
+
+export default ExpensesPage
 
