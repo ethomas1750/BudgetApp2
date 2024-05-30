7b4d6dce3abfab6c5ef2b59415f797918d64c136
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:35:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+const userRouter = require("./routes/user/userRouter")
+const serviceRouter = require("./routes/services/servicesRouter")
+const mailjetRouter = require("./routes/Mailjet/Mailjet")
 
+const app = express()
+
+app.use(logger("dev"))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use("/users/", userRouter)
+app.use("/service/",serviceRouter)
+app.use("/mailjet/",mailjetRouter)
+
+
+
+module.exports = app
