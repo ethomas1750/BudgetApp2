a4f05c3a86118614db00fa0f48e33a30bce39b81
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:24:18 -0500

    Update intro.jsx

--- intro.jsx
+++ intro.jsx
@@ -1,1 +1,45 @@
+import {Form} from "react-router-dom"
 
+// library
+import { UserPlusIcon } from "@heroicons/react/24/solid"
+
+// assets
+import {illustration} from "../assets/illustration.jpg"
+
+const Intro = () => {
+    return(
+        <div className="intro">
+            <div>
+                <h1>
+                    Take Control of <span className="accent"><Money></Money></span>
+                </h1>
+                <p>
+                    Personal budgeting is the secret to financial freedom. Start your journey today.
+                </p>
+                <Form method="post">
+                    <input 
+                    type="text" 
+                    name="userName" 
+                    required placeholder="What is your name" 
+                    aria-label="Your name" 
+                    autoComplete="given-name"
+                    />
+                    </div>
+                    <input type= "hidden" name="_action" value="newUser"
+                    <button type="submit" className="btn btn--dark"> disabled= {isSubmitting}>
+            
+            isSubmitting ? <span>Submitting...</span>
+            : (
+                        
+                        <span>Create Budget</span>
+                    
+                        <UserPlusIcon width={20}/>
+                    </button>
+                </Form>
+            </div>
+
+            <img src={illustration} alt="Person with mobey"
+        </div>
+    )
+}
+export default Intro
