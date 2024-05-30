e666653a55d662918fd14eab1d0abf030e473f62
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:27:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+require('dotenv').config()
 
+const mongoose = require('mongoose')
+
+const app = require('./app')
+
+const port = 3000
+
+mongoose
+    .connect('mongodb://127.0.0.1:27017/budget-app')
+    .then(()=>{
+        app.listen(port, ()=>{
+            console.log(`server started on port ${port}`)
+            console.log('MongoDB connected')
+        })
+    }).catch((error)=>{
+        console.log(error.message)
+    })
