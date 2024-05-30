6491ec143117ddf30e859316c7d70b753326ca58
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:11:39 -0500

    Update Error.jsx

--- Error.jsx
+++ Error.jsx
@@ -1,1 +1,45 @@
+// reactions
+import {userRouteError, Link} from  "react-router-dom"
 
+/// rrd imports
+// library imports
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+
+const AddBudgetForm = () => {
+    const fetcher = useFetcher()
+    const isSubmitting = fetcher.state === "submitting"
+
+    const formRef = useRef();
+    const focusRef
+
+useEffect (() => {
+    if(isSubmitting){
+        formRef. current.reset() 
+        focusRef.current.focus()
+    }
+    },[isSubmitting]}
+
+        return (
+        <div className="error">
+            <h1>Uh oh! We've got a problem.</h1>
+            <p>{error.message error.statusText} </p>
+            <div className="flex-md">
+            <button className="btn btn--dark" onClick= {() =>navigate}>
+                <ArrowUturnLeftIcon width={20}/>
+                <span>Go Back </span>
+            </button>
+            </fetcher.form>
+            <Link
+            to="/"
+            className="btn btn--dark" 
+            ref={formRef}
+            ref={focusRef}
+            >
+            <HomeIcon width={20}/>
+            </Link>
+            </div>
+        </div>
+
+    )
+}
+export default Error
