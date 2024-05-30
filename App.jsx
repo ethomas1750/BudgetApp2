1fccd76364f70c88fd3448b1d9e9e56b602c03c9
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-05-30 13:16:10 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,58 @@
+import{
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"; 
+
+//Library
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'; Caculating
+
+// Layouts
+import Main from ".layouts/Main";
+
+// Actions
+import { logoutAction } from "actions./logout";
+
+// Routes
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
+import Error from ".pages/Error";
+import ExpensesPage,{expensesLoader} from "./pages/ExspensesPage";
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <Main/>,
+      loader: mainLoader,
+      errorElement: <Error/>,
+      children: [
+         
+          {
+              index: true,
+              element: <Dashboard/>,
+              loader: dashboardLoader,
+              action: dashboardAction,
+               errorElement: <Error/>
+          },
+          {
+              path: true,
+              element: <ExpensePage/>,
+              // loader: expensesLoader,
+               
+          },
+          {
+              path: "logout",
+              action: logoutAction
+          } 
+      ]
+  },
+]);
+
+function App() {
+  return <div className="App"> 
+  <RouterProvider router={router}/>
+  <ToastContainer/>
+ </div>;
+}
+
+export default App;
+
 
