1127672a0d90d1818bcebffed52207b5a5d38995
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-10 00:30:01 -0500

    Update Error.jsx

--- Error.jsx
+++ Error.jsx
@@ -1,45 +1,27 @@
-// reactions
-import {userRouteError, Link} from  "react-router-dom"
+import { useRouteError, Link, useNavigate } from "react-router-dom";
 
-/// rrd imports
 // library imports
-import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 
-const AddBudgetForm = () => {
-    const fetcher = useFetcher()
-    const isSubmitting = fetcher.state === "submitting"
+const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
 
-    const formRef = useRef();
-    const focusRef
-
-useEffect (() => {
-    if(isSubmitting){
-        formRef. current.reset() 
-        focusRef.current.focus()
-    }
-    },[isSubmitting]}
-
-        return (
-        <div className="error">
-            <h1>Uh oh! We've got a problem.</h1>
-            <p>{error.message error.statusText} </p>
-            <div className="flex-md">
-            <button className="btn btn--dark" onClick= {() =>navigate}>
-                <ArrowUturnLeftIcon width={20}/>
-                <span>Go Back </span>
-            </button>
-            </fetcher.form>
-            <Link
-            to="/"
-            className="btn btn--dark" 
-            ref={formRef}
-            ref={focusRef}
-            >
-            <HomeIcon width={20}/>
-            </Link>
-            </div>
-        </div>
-
-    )
-}
-export default Error
+  return (
+    <div className="error">
+      <h1>Uh oh! We’ve got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go Back</span>
+        </button>
+        <Link to="/" className="btn btn--dark">
+          <HomeIcon width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Error;
