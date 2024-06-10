cc9a954e89ab708b7dfad32c52d459cc9550ccfa
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-10 00:36:47 -0500

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,18 +1,33 @@
-// rrd imports
-import {redirect} from" react-router-dom"; Calculating
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import { deleteItem, getAllMatchingItems } from "../helpers";
 
-// library
-import {toast} from "react-toastify";
+export function deleteBudget({ params }) {
+  const history = useHistory();
 
-//helper
-import { deleteItem } from "../helpers";
-
-export async function logoutAction(){
-    // delete the user
+  try {
     deleteItem({
-       key: "userName"
-    })
-    toast.success("You've deleted your account")
-    // return redirect
-    return redirect("/")
+      key: "budgets",
+      id: params.id,
+    });
+
+    const associatedExpenses = getAllMatchingItems({
+      category: "expenses",
+      key: "budgetId",
+      value: params.id,
+    });
+
+    associatedExpenses.forEach((expense) => {
+      deleteItem({
+        key: "expenses",
+        id: expense.id,
+      });
+    });
+
+    toast.success("Budget deleted successfully!");
+  } catch (e) {
+    throw new Error("There was a problem deleting your budget.");
+  }
+
+  history.push("/");
 }
