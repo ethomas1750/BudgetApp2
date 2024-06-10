5423698e707e56210f888393572c85bc2d3adf04
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-10 00:24:57 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,58 +1,65 @@
-import{
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"; 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-//Library
-import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'; Caculating
+// Library
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Layouts
-import Main from ".layouts/Main";
+import Main, { mainLoader } from "./layouts/Main";
 
 // Actions
-import { logoutAction } from "actions./logout";
-
+// import { logoutAction } from "./actions/logout"; // This import is not used in App component
+// import { deleteBudget } from "./actions/deleteBudget";
 // Routes
 import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
-import Error from ".pages/Error";
-import ExpensesPage,{expensesLoader} from "./pages/ExspensesPage";
-const router = createBrowserRouter([
-  {
-      path: "/",
-      element: <Main/>,
-      loader: mainLoader,
-      errorElement: <Error/>,
-      children: [
-         
-          {
-              index: true,
-              element: <Dashboard/>,
-              loader: dashboardLoader,
-              action: dashboardAction,
-               errorElement: <Error/>
-          },
-          {
-              path: true,
-              element: <ExpensePage/>,
-              // loader: expensesLoader,
-               
-          },
-          {
-              path: "logout",
-              action: logoutAction
-          } 
-      ]
-  },
-]);
+import Error from "./pages/Error";
+import BudgetPage, { budgetAction, budgetLoader } from "./pages/BudgetPage";
+import ExpensesPage, {
+  expensesAction,
+  expensesLoader,
+} from "./pages/ExpensesPage";
+
+const router = (
+  <Routes>
+    <Route
+      path="/"
+      element={<Main />}
+      loader={mainLoader}
+      index={true}
+    >
+      <Route
+        path="dashboard"
+        element={<Dashboard />}
+        loader={dashboardLoader}
+        action={dashboardAction}
+      />
+      <Route
+        path="budget/:id"
+        element={<BudgetPage />}
+        loader={budgetLoader}
+        action={budgetAction}
+      >
+        <Route path="delete" element={<DeleteBudget />} />
+      </Route>
+      <Route
+        path="expenses"
+        element={<ExpensesPage />}
+        loader={expensesLoader}
+        action={expensesAction}
+      />
+      <Route path="logout" element={<Logout />} />
+    </Route>
+    <Route path="*" element={<Error />} />
+  </Routes>
+);
 
 function App() {
-  return <div className="App"> 
-  <RouterProvider router={router}/>
-  <ToastContainer/>
- </div>;
+  return (
+    <div className="App">
+      <Router>{router}</Router>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
-
-
