0c9ab4690c3eaa9ce6bfb7c4c893e181191004eb
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-10 00:31:05 -0500

    Update ExpenseItem.jsx

--- ExpenseItem.jsx
+++ ExpenseItem.jsx
@@ -1,30 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './ExpenseItem.css'; // Import the CSS file for styling the expense item
+// rrd imports
+import { Link, useFetcher } from "react-router-dom";
 
-const ExpenseItem = ({ expense }) => {
-    const { name, amount, date, category } = expense;
+// library import
+import { TrashIcon } from "@heroicons/react/24/solid";
 
-    return (
-        <div className="expense-item">
-            <h3 className="expense-name">{name}</h3>
-            <div className="expense-details">
-                <p><strong>Amount:</strong> ${amount.toFixed(2)}</p>
-                <p><strong>Date:</strong> {new Date(date).toLocaleDateString()}</p>
-                <p><strong>Category:</strong> {category}</p>
-            </div>
-        </div>
-    );
+// helper imports
+import {
+  formatCurrency,
+  formatDateToLocaleString,
+  getAllMatchingItems,
+} from "../helpers";
+
+const ExpenseItem = ({ expense, showBudget }) => {
+  const fetcher = useFetcher();
+
+  const budget = getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId,
+  })[0];
+
+  return (
+    <>
+      <td>{expense.name}</td>
+      <td>{formatCurrency(expense.amount)}</td>
+      <td>{formatDateToLocaleString(expense.createdAt)}</td>
+      {showBudget && (
+        <td>
+          <Link
+            to={`/budget/${budget.id}`}
+            style={{
+              "--accent": budget.color,
+            }}
+          >
+            {budget.name}
+          </Link>
+        </td>
+      )}
+      <td>
+        <fetcher.Form method="post">
+          <input type="hidden" name="_action" value="deleteExpense" />
+          <input type="hidden" name="expenseId" value={expense.id} />
+          <button
+            type="submit"
+            className="btn btn--warning"
+            aria-label={`Delete ${expense.name} expense`}
+          >
+            <TrashIcon width={20} />
+          </button>
+        </fetcher.Form>
+      </td>
+    </>
+  );
 };
-
-ExpenseItem.propTypes = {
-    expense: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        amount: PropTypes.number.isRequired,
-        date: PropTypes.string.isRequired,
-        category: PropTypes.string.isRequired,
-    }).isRequired,
-};
-
 export default ExpenseItem;
